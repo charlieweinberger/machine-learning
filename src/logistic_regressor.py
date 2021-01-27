f6cca76b3feda6ac9504064fd7d02a6b4bef14ba
charliebrown364 <364charliebrown@gmail.com>
2021-01-27 06:03:55 +0000

    Finished Assignment 49-1 Part B and C, which was adding an upper_bound to my logistic regressor and adding the interaction term into the logistic regressor.

--- src/logistic_regressor.py
+++ src/logistic_regressor.py
@@ -4,35 +4,32 @@ sys.path.append('src')
 from linear_regressor import LinearRegressor
 
 def ln(x):
-    return math.log(x) / math.log(math.e)
+    if x > 0:
+        return math.log(x) / math.log(math.e)
+    else:
+        print("x is currently {}, but it cannt be less than zero".format(x))
 
 class LogisticRegressor():
     
-    def __init__(self, dataframe, dependent_variable):
+    def __init__(self, dataframe, dependent_variable, upper_bound):
         self.dataframe = dataframe
         self.dependent_variable = dependent_variable
+        self.upper_bound = upper_bound
         self.coefficients = self.calculate_coefficients()
 
     def calculate_coefficients(self):
 
         y = self.dataframe.data_dict[self.dependent_variable]
-        self.dataframe.data_dict[self.dependent_variable] = [ln((1 / var_val) - 1) for var_val in y]
+        self.dataframe.data_dict[self.dependent_variable] = [ln((self.upper_bound / var_val) - 1) for var_val in y]
 
         logistic_regressor = LinearRegressor(self.dataframe, dependent_variable = self.dependent_variable)
         
         return logistic_regressor.coefficients
 
     def predict(self, this_dict):
-        
-        coefficients_keys = list(self.coefficients.keys())
-        coefficients = list(self.coefficients.values())
 
-        linear_prediction = coefficients[0]
+        linear_regressor = LinearRegressor(self.dataframe, dependent_variable = self.dependent_variable)
+        linear_prediction = linear_regressor.predict(this_dict)
 
-        for i in range(1, len(coefficients)):
-            key = coefficients_keys[i]
-            if key != self.dependent_variable:
-                linear_prediction += self.coefficients[key] * this_dict[key]
-
-        logistic_prediction = 1 / (1 + math.e ** linear_prediction)
+        logistic_prediction = self.upper_bound / (1 + math.e ** linear_prediction)
         return logistic_prediction
--- tests/test_logistic_regressor.py
+++ tests/test_logistic_regressor.py
@@ -6,14 +6,44 @@ from dataframe import DataFrame
 from linear_regressor import LinearRegressor
 from logistic_regressor import LogisticRegressor
 
+sm = 0.0001
+
 df = DataFrame.from_array(
-    [[1,0.2],
-     [2,0.25],
-     [3,0.5]],
-    columns = ['x','y']
+    [[sm, sm, 1, sm], 
+    [1, sm, 2, sm], 
+    [2, sm, 4, sm], 
+    [4, sm, 8, sm], 
+    [6, sm, 9, sm], 
+    [sm, 2, 2, sm], 
+    [sm, 4, 5, sm], 
+    [sm, 6, 7, sm], 
+    [sm, 8, 6, sm],
+    [2, 2, 0.1, 4],
+    [3, 4, 0.1, 12]],
+    columns = ['beef', 'pb', 'rating', 'beef * pb']
 )
 
-log_reg = LogisticRegressor(df, dependent_variable = 'y')
-ans = log_reg.predict({'x': 5})
-assert round(ans, 3) == 0.777, ans
-print("passed")
+regressor = LogisticRegressor(df, dependent_variable = 'rating', upper_bound = 10)
+coefficients = regressor.coefficients
+print("\ncoefficients:", coefficients)
+
+# part ii
+ii_dict = {
+                    'beef': 5,
+                    'pb': 0
+               }
+print("part ii:", regressor.predict(ii_dict))
+
+# part iii
+iii_dict = {
+                    'beef': 12,
+                    'pb': 0
+               }
+print("part iii:", regressor.predict(iii_dict))
+
+# part iv
+iv_dict = {
+                    'beef': 5,
+                    'pb': 5
+               }
+print("part iv:", regressor.predict(iv_dict))
