53036939c5f8a55fcee9c8dc2a340184f48b43fd
charliebrown364 <>
2021-04-30 02:41:33 +0000

    Finished K-Nearest Neighbors & Leave-One-Out Cross Validation for assignment 86

--- src/k_nearest_neighbors_classifier.py
+++ src/k_nearest_neighbors_classifier.py
@@ -1,38 +1,42 @@
-import sys
-sys.path.append('src')
-from dataframe import DataFrame
+import pandas as pd
 
 class KNearestNeighborsClassifier():
     
     def __init__(self, k):
         self.k = k
-        self.df = None
-        self.dv = None
     
     def fit(self, df, dv):
-        self.df, self.dv = df, dv
+        self.df = df
+        self.dv = dv
+        self.dv_index = self.df.columns[0].index(self.dv)
+        self.idvs = [elem for elem in self.df.columns if elem != self.dv]
+
+    def get_data(self):
+        return [list(elem) for elem in list(self.df.values)]
 
     def euclidean_distance_calculator(self, a_values, b_values):
-        radicand = sum([(a_values[i] - b_values[i]) ** 2 for i in range(len(a_values))])
-        return radicand ** 0.5
+        radicand_list = [(a_values[i] - b_values[i]) ** 2 for i in range(len(a_values))]
+        return sum(radicand_list) ** 0.5
 
     def compute_distances(self, observation):
 
         observation_values = list(observation.values())
-        ans = []
+        data = []
 
-        for this_list in self.df.to_array():
-            distance = self.euclidean_distance_calculator(observation_values, this_list[1:])
-            ans.append([distance, this_list[0]])
-        
-        return ans
-    
+        for this_list in self.get_data():
+            this_dv = this_list[self.dv_index]
+            this_list.remove(this_dv)
+            distance = self.euclidean_distance_calculator(observation_values, this_list)
+            data.append([distance, this_dv])
+
+        return pd.DataFrame(data, columns=['Distance', self.dv])
+
     def nearest_neighbors(self, observation):
-        new_data_dict = self.compute_distances(observation)
-        new_df = DataFrame.from_array(new_data_dict, columns=['distance', self.dv])
-        return new_df.order_by('distance', ascending=True).to_array()
+        new_df = self.compute_distances(observation)
+        return new_df.sort_values(by=['Distance'])
     
     def classify(self, observation):
-        ordered_array = self.nearest_neighbors(observation)
-        top_k = [ordered_array[i][1] for i in range(len(ordered_array)) if i < self.k]
-        return max(set(top_k), key=top_k.count)
+        ordered_df = self.nearest_neighbors(observation)
+        ans = ordered_df[self.dv][:self.k].mode()
+        str_ans = str(ans).split('\n')[0].split(' ')[4]
+        return str_ans
--- tests/test_k_nearest_neighbors_classifier.py
+++ tests/test_k_nearest_neighbors_classifier.py
@@ -1,85 +1,68 @@
-import sys
-sys.path.append('src')
-from dataframe import DataFrame
-from k_nearest_neighbors_classifier import KNearestNeighborsClassifier
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+import matplotlib.pyplot as plt
+plt.style.use('bmh')
 
-df = DataFrame.from_array(
-    [['Shortbread'  ,     0.14     ,       0.14     ,      0.28     ,     0.44      ],
-    ['Shortbread'  ,     0.10     ,       0.18     ,      0.28     ,     0.44      ],
-    ['Shortbread'  ,     0.12     ,       0.10     ,      0.33     ,     0.45      ],
-    ['Shortbread'  ,     0.10     ,       0.25     ,      0.25     ,     0.40      ],
-    ['Sugar'       ,     0.00     ,       0.10     ,      0.40     ,     0.50      ],
-    ['Sugar'       ,     0.00     ,       0.20     ,      0.40     ,     0.40      ],
-    ['Sugar'       ,     0.10     ,       0.08     ,      0.35     ,     0.47      ],
-    ['Sugar'       ,     0.00     ,       0.05     ,      0.30     ,     0.65      ],
-    ['Fortune'     ,     0.20     ,       0.00     ,      0.40     ,     0.40      ],
-    ['Fortune'     ,     0.25     ,       0.10     ,      0.30     ,     0.35      ],
-    ['Fortune'     ,     0.22     ,       0.15     ,      0.50     ,     0.13      ],
-    ['Fortune'     ,     0.15     ,       0.20     ,      0.35     ,     0.30      ],
-    ['Fortune'     ,     0.22     ,       0.00     ,      0.40     ,     0.38      ]],
-    columns = ['Cookie Type' ,'Portion Eggs','Portion Butter','Portion Sugar','Portion Flour' ]
+df = pd.DataFrame(
+    [['Shortbread', 0.14, 0.14, 0.28, 0.44],
+     ['Shortbread', 0.10, 0.18, 0.28, 0.44],
+     ['Shortbread', 0.12, 0.10, 0.33, 0.45],
+     ['Shortbread', 0.10, 0.25, 0.25, 0.40],
+     ['Sugar'     , 0.00, 0.10, 0.40, 0.50],
+     ['Sugar'     , 0.00, 0.20, 0.40, 0.40],
+     ['Sugar'     , 0.02, 0.08, 0.45, 0.45],
+     ['Sugar'     , 0.10, 0.15, 0.35, 0.40],
+     ['Sugar'     , 0.10, 0.08, 0.35, 0.47],
+     ['Sugar'     , 0.00, 0.05, 0.30, 0.65],
+     ['Fortune'   , 0.20, 0.00, 0.40, 0.40],
+     ['Fortune'   , 0.25, 0.10, 0.30, 0.35],
+     ['Fortune'   , 0.22, 0.15, 0.50, 0.13],
+     ['Fortune'   , 0.15, 0.20, 0.35, 0.30],
+     ['Fortune'   , 0.22, 0.00, 0.40, 0.38],
+     ['Shortbread', 0.05, 0.12, 0.28, 0.55],
+     ['Shortbread', 0.14, 0.27, 0.31, 0.28],
+     ['Shortbread', 0.15, 0.23, 0.30, 0.32],
+     ['Shortbread', 0.20, 0.10, 0.30, 0.40]],
+     columns = ['Cookie Type', 'Portion Eggs', 'Portion Butter', 'Portion Sugar', 'Portion Flour']
     )
 
-knn = KNearestNeighborsClassifier(k=5)
-knn.fit(df, dv = 'Cookie Type')
+accuracies = []
+k_values = range(1, 19)
 
-observation = {
-    'Portion Eggs': 0.10,
-    'Portion Butter': 0.15,
-    'Portion Sugar': 0.30,
-    'Portion Flour': 0.45
-}
+def df_to_list(df):
+    return df.to_numpy().tolist()
 
-computed_distances = knn.compute_distances(observation) # Returns a dataframe representation of the following array
-print("\ncomputed_distances:")
-for pair in computed_distances:
-    print([round(pair[0], 3), pair[1]])
+def row_prediction_is_correct(knn, i):
 
-"""
+    x_df = df[[col for col in df.columns if col != 'Cookie Type']]
+    y_df = df['Cookie Type']
 
-[
-  [0.047, 'Shortbread'],
-  [0.037, 'Shortbread'],
-  [0.062, 'Shortbread'],
-  [0.122, 'Shortbread'],
-  [0.158, 'Sugar'],
-  [0.158, 'Sugar'],
-  [0.088, 'Sugar'],
-  [0.245, 'Sugar'],
-  [0.212, 'Fortune'],
-  [0.187, 'Fortune'],
-  [0.396, 'Fortune'],
-  [0.173, 'Fortune'],
-  [0.228, 'Fortune']
-]
+    df_to_row = lambda a: df_to_list(a.iloc[[i]])[0]
+    current_values =         df_to_row(x_df)
+    current_classification = df_to_row(y_df)
+    
+    df_to_dataset = lambda a: df_to_list(a.drop([i]).reset_index(drop=True))
+    train = df_to_dataset(x_df)
+    test =  df_to_dataset(y_df)
 
-"""
+    other_knn = knn.fit(train, test)
+    other_classification = other_knn.predict([current_values])
 
-# Note: the above has been rounded to 3 decimal places for ease of viewing, but you should not round in your class.
+    return current_classification == other_classification
 
-nearest_neighbors = knn.nearest_neighbors(observation) # Returns a dataframe representation of the following array
-print("\nnearest_neighbors:")
-for pair in nearest_neighbors:
-    print([round(pair[0], 3), pair[1]])
+for k in k_values:
+    
+    knn = KNeighborsClassifier(n_neighbors = k)
+    
+    num_total = len(df_to_list(df))
+    num_correct = ['' for i in range(num_total) if row_prediction_is_correct(knn, i)]
+    
+    accuracy = len(num_correct) / num_total
+    accuracies.append(accuracy)
 
-"""
-
-[
-  [0.037, 'Shortbread'],
-  [0.047, 'Shortbread'],
-  [0.062, 'Shortbread'],
-  [0.088, 'Sugar'],
-  [0.122, 'Shortbread'],
-  [0.158, 'Sugar'],
-  [0.158, 'Sugar'],
-  [0.173, 'Fortune'],
-  [0.187, 'Fortune'],
-  [0.212, 'Fortune'],
-  [0.228, 'Fortune'],
-  [0.245, 'Sugar'],
-  [0.396, 'Fortune']
-]
-
-"""
-
-assert knn.classify(observation) == 'Shortbread' # because this is the majority class in the 5 nearest neighbors
+plt.plot(k_values, accuracies)
+plt.xlabel('k')
+plt.ylabel('accuracy')
+plt.xticks(k_values)
+plt.title('Leave One Out Cross Validation')
+plt.savefig('leave_one_out_accuracy.png')
