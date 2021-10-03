41b3a5efc06223cccc86443a98657eb33ef982b2
Charlie Weinberger <70112746+charliebrown364@users.noreply.github.com>
2021-10-03 08:16:43 +0000

    Finished semirandom_dataset

--- datasets/semirandom_dataset.py
+++ datasets/semirandom_dataset.py
@@ -1,14 +1,17 @@
 import random
+import math
 import numpy as np
 import matplotlib.pyplot as plt
 plt.style.use('bmh')
 
-def get_coord(power, center):
-    population = [round(0.1*x, 1) for x in range(10 * (center - farthest), 10 * (center + farthest + 1))]
-    left_weights  = [power ** (x - center) for x in population[:len(population) // 2]]
-    right_weights = [power ** (center - x) for x in population[len(population) // 2:]]
-    weights = [x / sum(left_weights + right_weights) for x in left_weights + right_weights]
-    return random.choices(population, weights)
+def get_dist(power, farthest):
+    population = [round(x/100, 2) for x in range(int(100 * farthest))]
+    weights = [power ** (-x) for x in population]
+    weights = [x / sum(weights) for x in weights]
+    return random.choices(population, weights)[0]
+
+def get_angle():
+    return 2 * math.pi * random.random()
 
 def avg(x):
     return round(sum(x) / len(x), 3)
@@ -22,17 +25,26 @@ def semirandom_dataset(centers, classes, colors, farthest, total_num_points, pow
         
         center = centers[i]
         for _ in range(total_num_points // num_sets):
-            x, y = get_coord(power, center[0]), get_coord(power, center[1])
-            point = {'x': x[0], 'y': y[0], 'class': classes[i]}
+
+            d, theta = get_dist(power, farthest), get_angle()
+            
+            dx = d * math.cos(theta)
+            dy = d * math.sin(theta)
+            
+            x = center[0] + dx
+            y = center[1] + dy
+
+            point = {'x': x, 'y': y, 'class': classes[i]}
             points[i].append(point)
 
-        x_points = [point['x'] for point in points[i]]
-        y_points = [point['y'] for point in points[i]]
-
-        print('\ncenter:    ', center)
-        print('est_center:', (avg(x_points), avg(y_points)))
-
         if show:
+
+            x_points = [point['x'] for point in points[i]]
+            y_points = [point['y'] for point in points[i]]
+
+            print('\ncenter:    ', center)
+            print('est_center:', (avg(x_points), avg(y_points)))
+
             plt.scatter(x_points, y_points, c=colors[i])
 
     if show:
@@ -42,13 +54,12 @@ def semirandom_dataset(centers, classes, colors, farthest, total_num_points, pow
 
 centers = [(1, 4), (4, 1), (1, 1), (4, 4)]
 classes = [0, 0, 1, 1]
-colors = ['#0000FF', '#00FF00', '#FF0000', '#800080']
-farthest = 3
+colors = ['#0000FF', '#0000FF', '#FF0000', '#FF0000']
 total_num_points = 200
-power = 5
+
+farthest = 2.1
+power = 1.5
 
 points = semirandom_dataset(centers, classes, colors, farthest, total_num_points, power, show=True)
 
-'''
-python datasets/semirandom_dataset.py
-'''
+# python datasets/semirandom_dataset.py
